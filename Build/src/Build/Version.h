cc1e5bf4a4230dd3f8b66a973427f3233b4677d4
jiayinf <jiayinf@andrew.cmu.edu>
2025-12-05 09:46:29 +0800

    12.05 平移拖拽轨迹显示和距离正常，但位置不对

--- Build/src/Build/Version.h
+++ Build/src/Build/Version.h
@@ -6,11 +6,11 @@
 #define FCVersionPoint  "2"
 #define FCVersionSuffix ""
 // test: $Format:Hash (%H), Date: %ci$
-#define FCRevision      "38641 +678 (Git)"      //Highest committed revision number
-#define FCRevisionDate  "2025/08/05 20:19:07"     //Date of highest committed revision
-#define FCRepositoryURL "Unknown"      //Repository URL of the working copy
+#define FCRevision      "14555 (Git shallow)"      //Highest committed revision number
+#define FCRevisionDate  "2025/12/03 01:38:26"     //Date of highest committed revision
+#define FCRepositoryURL "git://github.com/Jiayinf/freeCAD.git main"      //Repository URL of the working copy
 
 // Git relevant stuff
-#define FCRepositoryHash   "256fc7eff3379911ab5daf88e10182c509aa8052"
-#define FCRepositoryBranch "tag: 1.0.2"
+#define FCRepositoryHash   "4d1b4415d8ea4c328f6a6181c54c5fd5302a7dc5"
+#define FCRepositoryBranch "main"
 
